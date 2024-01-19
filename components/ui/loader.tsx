012f9ff5b05453ec0151e8c7873ff9bb88f4d5f0
Adam Kazai <kazai.adam@gmail.com>
2024-01-19 17:06:51 +0100

    feat: create loader component

---
+++ components/ui/loader.tsx
@@ -0,0 +1,7 @@
+const Loader = () => {
+    return (
+        <span className="inline-block w-12 h-12 border-4 border-purple-700 rounded-full animate-spin border-b-transparent"></span>
+    );
+};
+
+export default Loader;
