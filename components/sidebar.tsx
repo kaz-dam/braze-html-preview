187a8066fa687f4ff87edb6a71becf89db0d3616
Adam Kazai <kazai.adam@gmail.com>
2024-02-05 16:04:26 +0100

    fix: transition sidebar on mobile

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,23 +1,17 @@
 "use client";
 
-import { useCallback, useRef, useState } from "react";
+import { useRef } from "react";
 import SearchItem from "./search-item";
 import MobileHeader from "./ui/mobile-header";
 import ViewSelector from "./view-selector";
 
 const Sidebar = () => {
-    const [ isOpen, setIsOpen ] = useState<boolean>(false);
     const asideRef = useRef<HTMLDivElement>(null);
-
-    const openSidebarOnMobile = useCallback(() => {
-        asideRef.current?.classList.toggle("-left-full");
-        setIsOpen(!isOpen);
-    }, []);
 
     return (
         <>
-            <MobileHeader onClick={openSidebarOnMobile} isOpen={isOpen} />
-            <aside ref={asideRef} className="fixed top-0 sm:left-0 -left-full z-50 w-[320px] h-full bg-blue-950 text-white px-6 py-10 transition-all">
+            <MobileHeader sidebarRef={asideRef} />
+            <aside ref={asideRef} className="fixed top-0 left-0 sm:translate-x-0 -translate-x-full z-50 w-[320px] h-full bg-blue-950 text-white px-6 py-10 transition-all">
                 <h1 className="text-3xl text-center mb-6">{process.env.NEXT_PUBLIC_BRAND_NAME || "Brand"}</h1>
                 <SearchItem />
                 <ViewSelector />
--- components/ui/mobile-header.tsx
+++ components/ui/mobile-header.tsx
@@ -1,11 +1,23 @@
 "use client";
 
 import { PanelLeftOpen, PanelLeftClose } from "lucide-react";
+import { useState } from "react";
 
-const MobileHeader = ({ onClick, isOpen }: any) => {
+const MobileHeader = ({ sidebarRef }: any) => {
+    const [ isOpen, setIsOpen ] = useState<boolean>(false);
+
+    const openSidebarOnMobile = () => {
+        console.log(sidebarRef);
+        sidebarRef.current?.classList.toggle("-translate-x-full");
+        setIsOpen(!isOpen);
+    };
+    
     return (
-        <div className="sm:hidden fixed top-3 right-3 w-12 h-12 rounded bg-blue-950 text-white flex items-center justify-center shadow" onClick={onClick}>
-            {isOpen ? <PanelLeftClose size={32} /> : <PanelLeftOpen size={32} />}
+        <div className="sm:hidden fixed top-3 right-3 w-12 h-12 z-50 rounded bg-blue-950 text-white flex items-center justify-center shadow" onClick={openSidebarOnMobile}>
+            {isOpen ? 
+                <PanelLeftClose size={32} /> :
+                <PanelLeftOpen size={32} />
+            }
         </div>
     );
 };
