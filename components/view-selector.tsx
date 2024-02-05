d4752820dc99e91dc8c922089d2f95b871bb56b3
Adam Kazai <kazai.adam@gmail.com>
2024-02-05 13:54:34 +0100

    feat: add responsive view selector

--- components/view-selector.tsx
+++ components/view-selector.tsx
@@ -4,7 +4,6 @@ import * as RadioGroup from '@radix-ui/react-radio-group';
 import { usePreview } from "@/context/preview-context";
 import { View } from "@/types/component";
 import { Monitor, Smartphone, Tablet } from "lucide-react";
-import { FormEventHandler } from 'react';
 
 const ViewSelector = () => {
     const { setSelectedView } = usePreview();
@@ -14,27 +13,25 @@ const ViewSelector = () => {
     };
     
     return (
-        <>
+        <div className="my-8">
+            <h2 className="text-xl text-center">Responsive view</h2>
             <RadioGroup.Root defaultValue={`${View.MOBILE}`} onValueChange={onViewChange} aria-label="Responsive view">
                 <div className="grid grid-cols-3 gap-2 my-6">
-                    <RadioGroup.Item value={`${View.DESKTOP}`}>
-                        <RadioGroup.Indicator className="flex items-center justify-center rounded py-2 cursor-pointer transition-all text-black hover:bg-brand-primary hover:text-brand-secondary">
-                            <Monitor className="w-8 h-8" size={32} />
-                        </RadioGroup.Indicator>
+                    <RadioGroup.Item value={`${View.DESKTOP}`} className="relative h-11 rounded cursor-pointer transition-all hover:bg-slate-200 overflow-hidden">
+                        <RadioGroup.Indicator className="w-full h-full bg-slate-200 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+                        <Monitor className="w-8 h-8 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" size={32} />
                     </RadioGroup.Item>
-                    <RadioGroup.Item value={`${View.TABLET}`}>
-                        <RadioGroup.Indicator className="flex items-center justify-center rounded py-2 cursor-pointer transition-all hover:bg-brand-primary hover:text-brand-secondary">
-                            <Tablet className="w-8 h-8" />
-                        </RadioGroup.Indicator>
+                    <RadioGroup.Item value={`${View.TABLET}`} className="relative h-11 rounded cursor-pointer transition-all hover:bg-slate-200 overflow-hidden">
+                        <RadioGroup.Indicator className="w-full h-full bg-slate-200 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+                        <Tablet className="w-8 h-8 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
                     </RadioGroup.Item>
-                    <RadioGroup.Item value={`${View.MOBILE}`}>
-                        <RadioGroup.Indicator className="flex items-center justify-center rounded py-2 cursor-pointer transition-all hover:bg-brand-primary hover:text-brand-secondary">
-                            <Smartphone className="w-8 h-8" size={32} />
-                        </RadioGroup.Indicator>
+                    <RadioGroup.Item value={`${View.MOBILE}`} className="relative h-11 rounded cursor-pointer transition-all hover:bg-slate-200 overflow-hidden">
+                        <RadioGroup.Indicator className="w-full h-full bg-slate-200 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+                        <Smartphone className="w-8 h-8 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" size={32} />
                     </RadioGroup.Item>
                 </div>
             </RadioGroup.Root>
-        </>
+        </div>
     );
 };
 
