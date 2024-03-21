fd57d341fe90136da4210328a4cc9a677a7bda7c
Adam Kazai <kazai.adam@gmail.com>
2024-03-21 15:00:49 +0100

    feat: add label to view selectors

--- components/view-selector.tsx
+++ components/view-selector.tsx
@@ -17,17 +17,26 @@ const ViewSelector = () => {
             <h2 className="text-xl text-center">Responsive view</h2>
             <RadioGroup.Root defaultValue={`${View.MOBILE}`} onValueChange={onViewChange} aria-label="Responsive view">
                 <div className="grid grid-cols-3 gap-2 my-6">
-                    <RadioGroup.Item value={`${View.DESKTOP}`} className="relative h-11 rounded cursor-pointer transition-all hover:bg-brand-primary overflow-hidden">
+                    <RadioGroup.Item value={`${View.DESKTOP}`} className="relative rounded hover:bg-brand-primary h-16 cursor-pointer transition-all overflow-hidden" id="desktop-view">
                         <RadioGroup.Indicator className="w-full h-full bg-brand-primary absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
-                        <Monitor className="w-8 h-8 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" size={32} />
+                        <div className="flex flex-col items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                            <Monitor className="w-8 h-8" size={32} />
+                            <span className="text-sm">Desktop</span>
+                        </div>
                     </RadioGroup.Item>
-                    <RadioGroup.Item value={`${View.TABLET}`} className="relative h-11 rounded cursor-pointer transition-all hover:bg-brand-primary overflow-hidden">
+                    <RadioGroup.Item value={`${View.TABLET}`} className="relative h-16 rounded cursor-pointer transition-all hover:bg-brand-primary overflow-hidden">
                         <RadioGroup.Indicator className="w-full h-full bg-brand-primary absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
-                        <Tablet className="w-8 h-8 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+                        <div className="flex flex-col items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                            <Tablet className="w-8 h-8" />
+                            <span className="text-sm">Tablet</span>
+                        </div>
                     </RadioGroup.Item>
-                    <RadioGroup.Item value={`${View.MOBILE}`} className="relative h-11 rounded cursor-pointer transition-all hover:bg-brand-primary overflow-hidden">
+                    <RadioGroup.Item value={`${View.MOBILE}`} className="relative h-16 rounded cursor-pointer transition-all hover:bg-brand-primary overflow-hidden">
                         <RadioGroup.Indicator className="w-full h-full bg-brand-primary absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
-                        <Smartphone className="w-8 h-8 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" size={32} />
+                        <div className="flex flex-col items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                            <Smartphone className="w-8 h-8" size={32} />
+                            <span className="text-sm">Mobile</span>
+                        </div>
                     </RadioGroup.Item>
                 </div>
             </RadioGroup.Root>
